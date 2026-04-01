1779876524a11fbc0f57718a8642230f6add4339
Augusto <augusto.t.ibarrola@gmail.com>
2026-04-01 14:00:10 -0400

    refactor

---
+++ 9-spring-aop/src/main/java/com/infy/springessentials/__spring_aop/Application.java
@@ -0,0 +1,13 @@
+package com.infy.springessentials.__spring_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
